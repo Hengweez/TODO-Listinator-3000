b0c3f2f4de45ed614de7bcdebd5bd8eeaa97481f
Segfault (Core dumped) <tyiwei0@gmail.com>
2025-04-02 22:09:32 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Todo-Listinator 3000
+
+Welcome to the Todo-listinator 3000!
+
+It's a todo list that helps you keep track of your tasks.
+
+## Feature list
+
+1. Add tasks
+2. Delete tasks
+3. List tasks
+4. Mark tasks as complete
+5. Saves tasks for use in the future
+
+## File structure
+
+data/
+| completed_tasks.txt # Stores completed tasks
+| tasks.txt # Stores pending tasks
+| backup.zip # Because saving overwrites old data, we've provided a backup.
+main.py # Main scripts
+
+## Tasks
+
+Fix the listed bugs
+
+For developer head: Clone this repository, create a branch, fix the bugs, commit the stuff, then open a pull request and push.
+
+1. Clone this repository
+2. Create a branch
+3. Fix the bugs
+4. Commit the changes
+5. Open a pull request on the repository on rdevcca github
+
+## Reported bugs to be fixed
+
+1. Program only prints "Todo-listinator 3000" then stops doing anything
+2. Program prints an error message when loading for some reason
+3. Listing tasks lists completed tasks twice
+4. For some reason, marking task '-1' as complete marks 'Pray to Lucilla' as completed
---
+++ main.py
@@ -0,0 +1,101 @@
+def add_task(incomplete_tasks):
+    task = input("Please insert task: ")
+    incomplete_tasks.append(task)
+
+
+def list_tasks(pending_tasks, completed_tasks):
+    print("\nPending tasks:")
+    if len(pending_tasks) == 0:
+        print("None")
+    else:
+        for i, task in enumerate(completed_tasks):
+            print(f"{i + 1}: {task}")
+
+    print("\nCompleted tasks:")
+    if len(completed_tasks) == 0:
+        print("None")
+    else:
+        for i, task in enumerate(completed_tasks):
+            print(f"{i + 1}: {task}")
+    print()
+
+
+def mark_as_completed(pending_tasks, completed_tasks):
+    print("\nPending tasks:")
+    if len(pending_tasks) == 0:
+        print("None")
+    else:
+        for i, task in enumerate(pending_tasks):
+            print(f"{i + 1}: {task}")
+
+    taskNumber = int(input("\nPlease input task number: "))
+
+    completed_tasks.append(pending_tasks.pop(taskNumber - 1))
+
+
+def save(filename, lst):
+    string = "\n".join(lst)
+    with open(filename, "w") as file:
+        file.write(string)
+
+
+def delete_task(pending_tasks, completed_tasks):
+    taskStatus = input("\nIs the task you wish to delete completed? [y/N]: ")
+    relevantList = []
+    if taskStatus == "y":
+        relevantList = completed_tasks
+    else:
+        relevantList = pending_tasks
+
+    taskNumber = int(input("\nPlease input task number: "))
+
+    if taskNumber > 0 and taskNumber <= len(pending_tasks):
+        relevantList.pop(taskNumber - 1)
+
+
+def load_tasks(filename):
+    tasks = []
+    with open(filename) as file:
+        task = file.readline()
+        while task:
+            tasks.append(task.strip())
+    return tasks
+
+
+def main():
+
+    quit = False
+
+    print("Todo-listinator 3000\n")
+
+    tasks = load_tasks("data/tasks.txt")
+    completed_tasks = load_tasks("completed_tasks.txt")
+
+    while not quit:
+        print(
+            """a: Add task 
+l: List tasks 
+m: Mark task as completed
+d: Delete task
+q: Quit and save tasks
+"""
+        )
+        operation = input("Please select operation: ")
+
+        match operation[0]:
+            case "a":
+                add_task(tasks)
+            case "l":
+                list_tasks(tasks, completed_tasks)
+            case "m":
+                mark_as_completed(tasks, completed_tasks)
+            case "d":
+                delete_task(tasks, completed_tasks)
+            case "q":
+                save("data/tasks.txt", tasks)
+                save("data/completed_tasks.txt", completed_tasks)
+                quit = True
+
+
+if __name__ == "__main__":
+    main()
